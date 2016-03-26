a827dbf7972a6b97bfa0e5be5941b5219fe89094
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-03-27 01:57:57 +0200

    r7plus: Remove unused audio config files
    Change-Id: I1f30b5942a6e0011e1bc343e0c5260577104acb6

--- device.mk
+++ device.mk
@@ -27,9 +27,7 @@ $(call inherit-product, device/oppo/msm8939-common/msm8939.mk)
 # Audio configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:system/etc/mixer_paths_mtp.xml \
-    $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
-    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml
+    $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:system/etc/mixer_paths_mtp.xml
 
 # ACDB Loader calibration
 PRODUCT_COPY_FILES += \
