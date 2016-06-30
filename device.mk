955d1402ea2e70edc00b623d504b0278f7b7ffd9
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-06-30 19:31:51 +0300

    r7plus: Move GPS HAL back to msm8939-common
    * Also commonize GPS blobs
    
    Change-Id: Iedca3dc33d6c98433d9b8eb99d822af39f1f083d

--- device.mk
+++ device.mk
@@ -67,15 +67,6 @@ PRODUCT_PACKAGES += \
     fingerprint.msm8916 \
     fingerprintd
 
-# GPS
-PRODUCT_PACKAGES += \
-    gps.msm8916
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
-
 # Input configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
