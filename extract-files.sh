5d134b1e42d3b665c38c3cc2b3551ea5ff354da1
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-08-02 01:02:58 +0300

    r7plus: Use common blob list
    * Also use the new generic extract utils in vendor/cm
    
    Change-Id: Ibb5053e368e74907487e7f64916c4bf9e9d76575

--- extract-files.sh
+++ extract-files.sh
@@ -1,63 +1,25 @@
 #!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-COMMON=msm8939-common
-DEVICE=r7plus
-VENDOR=oppo
+set -e
 
-if [ $# -eq 0 ]; then
-    SRC=adb
-else
-    if [ $# -eq 1 ]; then
-        SRC=$1
-    else
-        echo "$0: bad number of arguments"
-        echo ""
-        echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-        echo ""
-        echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-        echo "the device using adb pull."
-        exit 1
-    fi
-fi
+export BITS=64
+export DEVICE=r7plus
+export DEVICE_COMMON=msm8939-common
+export VENDOR=oppo
 
-function extract() {
-    for FILE in `egrep -v '(^#|^$)' $1`; do
-        OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
-        FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
-        DEST=${PARSING_ARRAY[1]}
-        if [ -z $DEST ]; then
-            DEST=$FILE
-        fi
-        DIR=`dirname $DEST`
-        if [ ! -d $2/$DIR ]; then
-            mkdir -p $2/$DIR
-        fi
-        # Try CM target first
-        if [ "$SRC" = "adb" ]; then
-            adb pull /system/$DEST $2/$DEST
-            # if file does not exist try OEM target
-            if [ "$?" != "0" ]; then
-                adb pull /system/$FILE $2/$DEST
-            fi
-        else
-            if [ -z $SRC/system/$DEST ]; then
-                echo ":: $DEST"
-                cp $SRC/system/$DEST $2/$DEST
-            else
-                echo ":: $FILE"
-                cp $SRC/system/$FILE $2/$DEST
-            fi
-        fi
-    done
-}
-
-COMMON_BASE=../../../vendor/$VENDOR/$COMMON/proprietary-64
-rm -rf $COMMON_BASE/*
-
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $BASE/*
-
-extract ../../$VENDOR/$COMMON/proprietary-files-64.txt $COMMON_BASE
-extract ../../$VENDOR/$DEVICE/proprietary-files.txt $BASE
-
-./setup-makefiles.sh
+./../$DEVICE_COMMON/extract-files.sh $@
