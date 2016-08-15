22e7aedeaf6b86616bed471192ab36a013c620ea
Danny Baumann <dannybaumann@web.de>
2016-08-15 08:34:53 +0200

    r7plus has 3GB of RAM, so use the according configs.
    Change-Id: Ie34ae40c860862a0147ddb794c877556ecd2d759

--- device.mk
+++ device.mk
@@ -45,8 +45,8 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Dalvik/HWUI configs
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
 
 # Density
 PRODUCT_AAPT_CONFIG := normal
